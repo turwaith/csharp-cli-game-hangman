57fad1f5e3b3d8e721aee762d6ba09c33c6ab154
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-09-14 23:07:04 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+    A tiny hangman console game
+*/
+using System;
+
+namespace csharp_game_hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
