f10439c0a2de08bd7148eb97fbdf1c9c2f9b9ad5
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-09-16 22:33:25 +0200

    Add basic code

--- Program.cs
+++ Program.cs
@@ -9,7 +9,33 @@ namespace csharp_game_hangman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] dictionnary = {"computer"};
+
+            for(int i = 0, dicoSize = dictionnary.Length; i < dicoSize; i++)          
+            {
+                char[] word = dictionnary[i].ToCharArray();
+                char[] hangman = new char[word.Length];
+
+                for(int j = 0; j < word.Length; j++) hangman[j] = '-';
+
+                while(true)
+                {
+                    Console.WriteLine($"Mot à trouver {String.Join("", hangman)}");
+                    Console.Write("Tapez votre lettre ");
+                    ConsoleKeyInfo userGuess = Console.ReadKey();
+                    for(int k = 0; k < word.Length; k++)
+                    {
+                        if(userGuess.KeyChar == word[k]) hangman[k] = userGuess.KeyChar;
+                    }
+                    Console.WriteLine();
+                    if(String.Join("", word) == String.Join("", hangman)) 
+                    {
+                        Console.WriteLine("Bien joué");
+                        break;
+                    }                   
+                }
+            }
+
         }
     }
 }
